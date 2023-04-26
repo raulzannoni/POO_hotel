ba0d146f12a34cf41decc4af4d7bf97a1238f103
raulzannoni <raul.zannoni92@gmail.com>
2023-04-26 11:27:14 +0200

    client date update

--- classes/Client.php
+++ classes/Client.php
@@ -50,28 +50,46 @@ class Client
         //methode pour afficher les reservations de le client
         public function getReservations()
             {
-                //titre
-                $result =   "<h4>Réservation de ".$this."</h4>";
+                //titre   
+                ?>
+                    <h4>Réservation de <?=$this ?></h4>
+                <?php
                 //controle s'il n'y a pas de reservation
                 if(count($this->_reservations) == 0)
                     {
-                        $result .= "Aucune Reservation !";
+                        ?>
+                            Aucune Reservation !
+                        <?php
                     }
                 else
                     {
                         //singulier ou pluriel
                         $ReservationS = (count($this->_reservations) > 1) ? "RESERVATIONS" : "RESERVATION";
+                        
                         //nombre de les reservations
-                        $result .=  count($this->_reservations)." ".$ReservationS."<br>";
+                        ?>
+                            <p><?=count($this->_reservations)." ".$ReservationS?><br></p>
+                        <?php
+
                         //le reservations de ce client
                         foreach($this->_reservations as $reservation)
                             {
-                                $result .= $reservation->getChambre()->getHotel()." /  Chambre ".$reservation->getChambre()->getIndex()."<br>";
+                                //index de la chambre
+                                $index = $reservation->getChambre()->getIndex();
+
+                                
+                                //controle de le wifi de la chambre
+                                $wifi = ($reservation->getChambre()->getHotel()->listChambres()[$index]->getWifi()) ? "oui" : "non";
+
+                                //prix par jour de la chambre
+                                $prixJour = $reservation->getChambre()->getHotel()->listChambres()[$index]->getPrixJour();
+                                ?>
+                                <b><?= $reservation->getChambre()->getHotel()?></b> / <?=$reservation->getChambre()?>
+                                <?="(". $prixJour." € - Wifi :  ".$wifi.
+                                " du ".$reservation->getDateDebut()->format('d-m-Y').
+                                " au ".$reservation->getDateSortie()->format('d-m-Y') ?><br>
+                                <?php
                             }
-                    }
-                //imprime de les reservations
-                echo $result;            
+                    }      
             }
-
-
     }
